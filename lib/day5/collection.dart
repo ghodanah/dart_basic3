8f0b6ec81fac6216b1a28db7f88df96c7b749a88
Jiyeon Han <hhh1529@gmail.com>
2023-12-04 18:53:13 +0900

    23.12.04. 과제 연습문제 2-3

--- lib/day5/collection.dart
+++ lib/day5/collection.dart
@@ -4,15 +4,19 @@
 // 대한민국의 도시별 인구수 (순서 상관 없음) - Map
 
 //이름을 가지는 Person 클래스를 작성하시오. Person은 반드시 이름을 포함해야 합니다.
+import 'package:untitled2/day2/variables_1.dart';
+
 class Person {
   String name;
+  int age;
 
   Person({
     required this.name,
+    required this.age
   });
 
-  void printInfo(){
-    print('$name');
+  String word(){
+    return('$name의 나이는 $age살');
   }
 
 }
@@ -20,14 +24,21 @@ class Person {
 void main() {
   //이름이 ‘홍길동', ‘한석봉' 인 Person 인스턴스를 생성하고, List에 담습니다.
   List<Person> person = [];
-  Person person1 = Person(name: '홍길동');
-  Person person2 = Person(name: '한석봉');
+  Person person1 = Person(name: '홍길동', age: 20);
+  Person person2 = Person(name: '한석봉', age: 25);
 
   person.add(person1);
   person.add(person2);
 
   for (Person person in person) {
     print(person.name);
+  }
+
+  Map<String, int> nameAge = {
+    for (Person person in person) person.name: person.age
+  };
+  for (Person person in person){
+     print(person.word());
   }
 }
 
