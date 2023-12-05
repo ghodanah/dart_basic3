5c2c136141b8eb392d3659bc7a3360c7c54c94e6
Jiyeon Han <hhh1529@gmail.com>
2023-12-05 16:01:26 +0900

    23.12.5. 연습문제 11-3

---
+++ lib/game/Hero.dart
@@ -0,0 +1,43 @@
+import 'Slime.dart';
+import 'Sword.dart';
+
+class Hero {
+  String name;
+  int _hp;
+  Sword? sword;
+
+  Hero({
+    required this.name,
+    required int hp,
+    this.sword,
+  }) : _hp = hp;
+
+  // getter
+  // 프로퍼티
+  int get hp => _hp;
+
+  set hp(int value) {
+    if (value < 0) {
+      throw Exception('hp는 0보다 작을 수 없다');
+    }
+
+    _hp = value;
+  }
+
+  void attack(Slime slime) {
+    slime.hp -= 10;
+    print('10의 데미지!!!');
+  }
+
+  void run() {
+    print('뛰었다');
+  }
+}
+
+void main() {
+  final hero = Hero(name: '용사', hp: 100);
+  hero.name = 'aa';
+  print(hero.hp);
+  hero.hp = 100;
+
+}
---
+++ lib/game/PoisonSlime.dart
@@ -0,0 +1,38 @@
+import 'package:untitled2/game/Hero.dart';
+import 'package:untitled2/game/Slime.dart';
+
+// 독슬라임: 슬라임 중에서도 독 공격이 되는 슬라임
+class PoisonSlime extends Slime {
+
+  PoisonSlime({required super.suffix});
+
+  int possibleAttack = 5;
+
+  @override
+  void attack(Hero hero) {
+    print('슬라임$suffix이/가 공격했다.');
+    print('10의 데미지');
+    hero.hp -= 10;
+
+    int currentHP = hero.hp;
+    int poisonDamage = currentHP ~/ 5;
+
+    if(possibleAttack > 0) {
+      print('추가로 독 포자를 살포했다!');
+      hero.hp -= poisonDamage;
+      print('$poisonDamage포인트의 데미지');
+      possibleAttack--;
+    }
+  }
+}
+
+
+void main() {
+  final poisonSlime = PoisonSlime(suffix: '독슬라임');
+
+  final hero = Hero(name: '홍길동', hp: 50);
+
+  poisonSlime.attack(hero);
+
+  poisonSlime.attack(hero);
+}
