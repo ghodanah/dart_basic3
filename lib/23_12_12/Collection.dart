e1a42986415d939a4a50af6bdb9ef5518ca03d54
Jiyeon Han <hhh1529@gmail.com>
2023-12-12 22:23:56 +0900

    23.12.12. collection 연습문제

---
+++ lib/23_12_12/Collection.dart
@@ -0,0 +1,71 @@
+class Collection {
+  List<CollectionChartDataList>? collectionChartDataList;
+
+  Collection({
+    this.collectionChartDataList,
+  });
+
+  Collection.fromJson(Map<String, dynamic> json) {
+    if (json['collectionChartDataList'] != null) {
+      collectionChartDataList = [];
+      json['collectionChartDataList'].forEach((v) {
+        collectionChartDataList?.add(CollectionChartDataList.fromJson(v));
+      });
+    }
+  }
+
+  Map<String, dynamic> toJson() {
+    final map = <String, dynamic>{};
+    if (collectionChartDataList != null) {
+      map['collectionChartDataList'] =
+          collectionChartDataList?.map((v) => v.toJson()).toList();
+    }
+    return map;
+  }
+}
+
+class CollectionChartDataList {
+  CollectionChartDataList({
+    this.collectionName,
+    this.collectionSalePrice,});
+
+  CollectionChartDataList.fromJson(dynamic json) {
+    collectionName = json['collectionName'];
+    collectionSalePrice = json['collectionSalePrice'];
+  }
+
+  String? collectionName;
+  dynamic collectionSalePrice;
+
+  Map<String, dynamic> toJson() {
+    final map = <String, dynamic>{};
+    map['collectionName'] = collectionName;
+    map['collectionSalePrice'] = collectionSalePrice;
+    return map;
+  }
+
+}
+
+void main() {
+  final collection = Collection.fromJson ({
+    "collectionChartDataList": [
+      {
+        "collectionName": "collectionName",
+        "collectionSalePrice": null
+      },
+      {
+        "collectionName": "collectionName",
+        "collectionSalePrice": [
+          {
+            "price": 59.75,
+            "cvtDatetime": "2023-03-26T08:08:35"
+          },
+          {
+            "price": 60.00,
+            "cvtDatetime": "2023-03-26T08:08:45"
+          }
+        ]
+      }
+    ]
+  }''';
+}
